f66e823b46ec2132898e5ae1400435c5feabe277
Oliviagiles09 <giles11@usf.edu>
2025-02-09 21:10:26 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,10 @@
+// Task 1
+let customer = {
+name: "Natalie Rob",
+age: 25,
+email: "Natalie.R@email.com"
+};
+
+consol.log('Name: ${customer.name}');
+consol.log('Age: ${customer.age}');
+consol.log('Email: ${customer.email}');
