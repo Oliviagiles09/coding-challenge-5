d38ad7a40318e864e2cde73d09154f3df0bdd94e
Oliviagiles09 <giles11@usf.edu>
2025-02-09 21:16:21 -0500

    Task 2 - Order Details

--- cc_5.js
+++ cc_5.js
@@ -8,3 +8,18 @@ email: "Natalie.R@email.com"
 consol.log('Name: ${customer.name}');
 consol.log('Age: ${customer.age}');
 consol.log('Email: ${customer.email}');
+
+// Task 2
+let order = {
+  orderID: 12345,
+  totalAmount: 150,
+  status: "Processing",
+
+  displayOrder: function() {
+    consol.log('Order ID: ${this.orderID}, Amount: $${this.totalAmount}, Status: ${this.status}');
+  }
+
+};
+
+order.displayOrder();
+
